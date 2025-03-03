d263205e777939b406c9c7b210a146be36dd63a7
Luca Giussani <lucagiussani@horizenlabs.io>
2025-03-03 12:19:44 +0100

    Move verifier-specific contracts to their own directory

--- contracts/verifiers/ZkVerifyGroth16.sol
+++ contracts/verifiers/ZkVerifyGroth16.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "./interfaces/IVerifyProofAggregation.sol";
+import "../interfaces/IVerifyProofAggregation.sol";
 
 contract ZkVerifyGroth16 {
     IVerifyProofAggregation immutable zkVerifyAggregation;
--- contracts/verifiers/ZkVerifyRisc0.sol
+++ contracts/verifiers/ZkVerifyRisc0.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "./interfaces/IVerifyProofAggregation.sol";
+import "../interfaces/IVerifyProofAggregation.sol";
 
 contract ZkVerifyRisc0 {
     IVerifyProofAggregation immutable zkVerifyAggregation;
